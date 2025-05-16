593edab3bcc49eb049c1122db69a3002841fec88
kobbin003 <dkfeto@gmail.com>
2025-05-16 13:10:01 +0530

    verceljson file added

--- src/components/SearchHospital/Bookings.jsx
+++ src/components/SearchHospital/Bookings.jsx
@@ -7,23 +7,6 @@ import TimeCapsule from "./TimeCapsule";
  * index 1 -> tommorrow
  * index 2 -> day after tommorrow
  */
-// const demoBookingTimings = [
-// 	{
-// 		morning: ["11:30 AM"],
-// 		afternoon: ["12:00 PM", "12:30 PM", "01:30 PM", "02:00 PM", "02:30 PM"],
-// 		evening: ["06:00 PM", "06:30 PM", "07:00 PM", "07:30 PM"],
-// 	},
-// 	{
-// 		morning: ["11:30 AM"],
-// 		afternoon: ["12:00 PM", "12:30 PM", "01:30 PM", "02:00 PM", "02:30 PM"],
-// 		evening: ["06:00 PM", "06:30 PM", "07:00 PM", "07:30 PM"],
-// 	},
-// 	{
-// 		morning: ["11:30 AM"],
-// 		afternoon: ["12:00 PM", "12:30 PM", "01:30 PM", "02:00 PM", "02:30 PM"],
-// 		evening: ["06:00 PM", "06:30 PM", "07:00 PM", "07:30 PM"],
-// 	},
-// ];
 const bookingTimeSlots = {
 	morning: [{ time: "11:30 AM", available: true }],
 	afternoon: [
